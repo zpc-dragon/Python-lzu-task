794d0837e8fe2fe54dddb28a84eb50f9eb9010fb
张鹏春 <102280098+zpc-dragon@users.noreply.github.com>
2023-12-17 21:24:05 +0800

    文件删除

--- OCSIP/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import data
-
-class dataAdmin(admin.ModelAdmin):
-    list_display = ['username', 'token', 'dataset_name', 'dataset_created_time',
-                    'dataset_updated_time', 'img_name', 'img_created_time', 'img_class', 'img_show']
-    list_filter = ['username','dataset_name']
-
-admin.site.register(data, dataAdmin)
--- OCSIP/home/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-# from .models import UploadedImage
-# Create your models here.
-from django.contrib.auth.models import User
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
